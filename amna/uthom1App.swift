074bee1d3dbbe50bc8a6fa9d256b5bd73a34cab0
Aeshah <amohammedalabdulkarim25@twq.idserve.net>
2025-12-08 15:00:03 +0300

    changed the app name

--- amna/uthom1App.swift
+++ amna/uthom1App.swift
@@ -9,7 +9,6 @@
 
 import SwiftUI
 
-@main
 struct uthom1App: App {
     
     
--- team15/team15App.swift
+++ team15/team15App.swift
@@ -1,1 +1,14 @@
 
+import SwiftUI
+
+@main
+struct Team15App: App {
+    @StateObject var languageVM = LanguageViewModel()
+
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()             // FIRST SCREEN OF YOUR APP
+                .environmentObject(languageVM)
+        }
+    }
+}
